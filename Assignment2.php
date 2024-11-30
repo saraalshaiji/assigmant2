bfbed92a430b25d11c567dcdb239c06eaabff676
saraalshaiji <sara.alshaiji188@gmail.com>
2024-11-30 19:53:17 +0300

    Update Assignment2.php

--- Assignment2.php
+++ Assignment2.php
@@ -20,3 +20,70 @@ if (!$data_output || !isset($data_output["results"])) {
 $output = $data_output["results"];
 
 ?> 
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="color-scheme" content="light dark">
+    <title> UOB Student Nationality Data </title>
+    <link rel="stylesheet" href="css/pico.min.css">
+    <style>
+        
+        /* table Styling  */
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+
+        table th, table td {
+            padding: 10px; 
+            text-align: left;
+            border: 1px solid #ddd; 
+        }
+
+        table th {
+            background-color: #f2f2f2; 
+        }
+
+        table tbody tr:nth-child(even) {
+            background-color: #f9f9f9; 
+        }
+    </style>
+</head>
+
+<body>
+
+<!-- Display the data in the table -->
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th> Year </th>
+                <th> Semester </th>
+                <th> The Programs </th>
+                <th> Nationality </th>
+                <th> Colleges </th>
+                <th> Number of students </th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <?php
+            foreach ($output as $student) {
+                ?>
+                <tr>
+                 
+                 <!-- Null operator for handling the undefined data -->
+                    <td><?php echo $student["year"] ?? 'N/A'; ?></td>
+                    <td><?php echo $student["semester"] ?? 'N/A'; ?></td>
+                    <td><?php echo $student["the_programs"] ?? 'N/A'; ?></td>
+                    <td><?php echo $student["nationality"] ?? 'N/A'; ?></td>
+                    <td><?php echo $student["colleges"] ?? 'N/A'; ?></td>
+                    <td><?php echo $student["number_of_students"] ?? 'N/A'; ?></td>
+                </tr>
+            <?php 
+            }
+            ?>
+        </tbody>
+    </table>
+</body>
+</html>
